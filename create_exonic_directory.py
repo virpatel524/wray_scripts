735e4bdc2b241c0ace58098a85c5918144d38d6f
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 16:50:24 -0400

    routine upload

--- create_exonic_directory.py
+++ create_exonic_directory.py
@@ -1,3 +1,11 @@
 import os
 import csv
 
+rootdir = '/home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes'
+
+for subdir, dirs, files in os.walk(rootdir):
+	sortedfles = sorted(files)
+	if len(sortedfles)!=2:
+		continue
+	tmp = list(csv.reader(open(sortedfles[0]),delimiter='\t'))
+	print tmp
