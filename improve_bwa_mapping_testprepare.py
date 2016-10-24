ad33d500614b7c9bf9bc43d73de09bcbc745a43d
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 12:26:23 -0400

    routine upload

--- improve_bwa_mapping_testprepare.py
+++ improve_bwa_mapping_testprepare.py
@@ -21,10 +21,14 @@ for filename in os.listdir('/home/vdp5/data/cambodia_samples/sequences_gz'):
 	item_dict.setdefault(sample_id, []).append(os.path.join('/home/vdp5/data/cambodia_samples/sequences_gz', filename))
 
 
+klist = [5, 10, 15, 20, 30, 50, 100]
+lowerwlist = [25, 50, 100, 150, 200]
+dlist = [25, 50, 100, 150, 200]
+clist = [0,25,50,100,150,200]
 current_number = 1
 for alpha in range(1, len(item_dict.keys()) + 1):
 	if alpha % 10 == 0:
 		current_number+=1
-	with open('cambodia_align_scripts/fastqgz_to_sam_cambodia_%d.sh' %(current_number), 'a') as abouttoputin:
+	with open('/home/vdp5/scripts/cambodia_PV01_improve_align/fastqgz_to_sam_cambodia_%d_improve.sh' %(current_number), 'a') as abouttoputin:
 		beta = item_dict.keys()[alpha - 1]	
 		abouttoputin.write('bwa mem -M /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta %s %s | samtools view  -Sb - | samtools sort - /home/vdp5/data/cambodia_samples/sequences_bam/%s.sorted.bam\n\n' %(item_dict[beta][0], item_dict[beta][1], beta.split('.')[0]))
