9ab353c637b00e2fd13517b800398e1f18209c56
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 12:31:30 -0400

    routine upload

--- improve_bwa_mapping_testprepare.py
+++ improve_bwa_mapping_testprepare.py
@@ -1,6 +1,7 @@
 import csv
 import sys, os
 import subprocess
+import itertools
 
 
 
@@ -23,12 +24,15 @@ for filename in os.listdir('/home/vdp5/data/cambodia_samples/sequences_gz'):
 
 klist = [5, 10, 15, 20, 30, 50, 100]
 lowerwlist = [25, 50, 100, 150, 200]
+rlist = [0.25, 0.5, 1.0, 1.5, 1.75, 2.0]
 dlist = [25, 50, 100, 150, 200]
 clist = [0,25,50,100,150,200]
+
 current_number = 1
 for alpha in range(1, len(item_dict.keys()) + 1):
 	if alpha % 10 == 0:
 		current_number+=1
-	with open('/home/vdp5/scripts/cambodia_PV01_improve_align/fastqgz_to_sam_cambodia_%d_improve.sh' %(current_number), 'a') as abouttoputin:
-		beta = item_dict.keys()[alpha - 1]	
-		abouttoputin.write('bwa mem -M /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta %s %s | samtools view  -Sb - | samtools sort - /home/vdp5/data/cambodia_samples/sequences_bam/%s.sorted.bam\n\n' %(item_dict[beta][0], item_dict[beta][1], beta.split('.')[0]))
+	for listsel in itertools.product(klist, lowerwlist, dlist, clist):	
+		with open('/home/vdp5/scripts/cambodia_PV01_improve_align/fastqgz_to_sam_cambodia_%d_improve.sh' %(current_number), 'a') as abouttoputin:
+			beta = item_dict.keys()[alpha - 1]	
+			abouttoputin.write('bwa mem -M -k %f /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta %s %s | samtools view  -Sb - | samtools sort - /home/vdp5/data/cambodia_samples/sequences_bam/%s.sorted.bam\n\n' %(item_dict[beta][0], item_dict[beta][1], beta.split('.')[0]))
