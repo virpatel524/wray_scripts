4949eb1a9ab2a4218cb1132f71042598d74310c9
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 17:36:09 -0400

    routine upload

--- create_exonic_directory.py
+++ create_exonic_directory.py
@@ -3,7 +3,7 @@ import csv
 
 exitfle = open('/home/vdp5/data/cdna_analysis_SAMEA2376790/gene2exon_PV01.txt', 'w')
 rootdir = '/home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes'
-
+flag_file = open('/home/vdp5/data/cdna_analysis_SAMEA2376790/flagged_genes.txt', 'w')
 
 gene2exons = {}
 
@@ -12,7 +12,8 @@ for subdir, dirs, files in os.walk(rootdir):
 	if len(sortedfles)!=2:
 		continue
 	tmp = list(csv.reader(open(os.path.join(subdir, sortedfles[0])),delimiter='\t'))
-	gene=sortedfles[0].split('_')[1]
+	gene='_'.join(sortedfles[0].split('_')[:2])
+	exonnums = []
 	tmplst = []
 	for alpha in tmp:
 		if len(alpha) == 0: continue
@@ -22,4 +23,14 @@ for subdir, dirs, files in os.walk(rootdir):
 				continue 
 			tmplst.append('exon{}:{}'.format(data[2], data[3].split('(')[-1].split(',')[0]))
 
-	print sorted(tmplst), sortedfles[0]
+			exonnums.append(int(data[2]))
+	tmplst = sorted(tmplst)
+	if len(tmplst) != list(set(exonnums)):
+		flag_file.write('{}\n'.format(gene))
+		continue
+	exitfle.write('>{}\n'.format(gene))
+	exitfle.write('{}}'.format('\n'.join(tmplst)))
+
+
+exitfle.close()
+flag_file.close()
