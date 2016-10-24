85f57bbc5f9f5e25b56574eee2f4f0464001b2ab
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 12:52:01 -0400

    routine upload

--- improve_bwa_mapping_testprepare.py
+++ improve_bwa_mapping_testprepare.py
@@ -38,5 +38,8 @@ for alpha in range(1, len(item_dict.keys()) + 1):
 		print index, len(iterator)
 		with open('/home/vdp5/scripts/cambodia_PV01_improve_align/fastqgz_to_sam_cambodia_%d_improve.sh' %(current_number), 'a') as abouttoputin:
 			beta = item_dict.keys()[alpha - 1]	
-			abouttoputin.write('bwa mem -M -k {} -w {} -r {} -d {} -c {} /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta {} {} | samtools view  -Sb - | samtools sort - /home/vdp5/tmp/{}_k{}_w{}_r{}_d{}_c{}.sorted.bam\n'.format(listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], item_dict[beta][0], item_dict[beta][1], listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], beta.split('.')[0]))
-			newstr = 'qualimap bamqc -bam $file -outdir /$basecase -outfile $basecase.analysis.pdf -outformat pdf'
+			abouttoputin.write('bwa mem -M -k {} -w {} -r {} -d {} -c {} /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta {} {} | samtools view  -Sb - | samtools sort - /home/vdp5/tmp/{}_k{}_w{}_r{}_d{}_c{}.sorted.bam\n'.format(listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], item_dict[beta][0], item_dict[beta][1], beta.split('.')[0], listsel[0], listsel[1], listsel[2], listsel[3], listsel[4]))
+			abouttoputin.write('qualimap bamqc -bam /home/vdp5/tmp/{}_k{}_w{}_r{}_d{}_c{}.sorted.bam -outdir /home/vdp5/data/cambodia_samples/bam_align_quality_testing_improve/{} -outfile {}.analysis.pdf -outformat pdf\n'.format(beta.split('.')[0], listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], beta.split('.')[0], beta.split('.')[0]))
+			abouttoputin.write('rm -rf /home/vdp5/tmp/{}_k{}_w{}_r{}_d{}_c{}.sorted.bam'.format(beta.split('.')[0], listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], beta.split('.')[0], beta.split('.')[0]))
+
+		
