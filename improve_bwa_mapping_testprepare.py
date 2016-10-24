d1a2f56ecfd3f0f817fa59935cf616cc04fe0a6b
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 12:41:46 -0400

    routine upload

--- improve_bwa_mapping_testprepare.py
+++ improve_bwa_mapping_testprepare.py
@@ -33,7 +33,9 @@ for alpha in range(1, len(item_dict.keys()) + 1):
 	if alpha % 10 == 0:
 		current_number+=1
 		if current_number != 1: break
-	for listsel in itertools.product(klist, lowerwlist, rlist, dlist, clist):
+	iterator = list(itertools.product(klist, lowerwlist, rlist, dlist, clist))
+	for index, listsel in enumerate(iterator):
+		print index, len(iterator)
 		with open('/home/vdp5/scripts/cambodia_PV01_improve_align/fastqgz_to_sam_cambodia_%d_improve.sh' %(current_number), 'a') as abouttoputin:
 			beta = item_dict.keys()[alpha - 1]	
 			abouttoputin.write('bwa mem -M -k {} -w {} -r {} -d {} -c {} /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta {} {} | samtools view  -Sb - | samtools sort - /home/vdp5/data/cambodia_samples/sequences_bam/{}_k{}_w{}_r{}_d{}_c{}.sorted.bam\n\n'.format(listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], item_dict[beta][0], item_dict[beta][1], listsel[0], listsel[1], listsel[2], listsel[3], listsel[4], beta.split('.')[0]))
