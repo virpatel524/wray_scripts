7561870b6577420f0eef32f570e0effcd2ec75da
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-24 11:45:22 -0400

    routine upload

--- parse_SAMEA2376790_gb.py
+++ parse_SAMEA2376790_gb.py
@@ -7,16 +7,18 @@ import os
 for filename in os.listdir('/home/vdp5/data/gene_finder/SAMEA2376790_genbank/SAMEA2376790_gb/'):
 	chromname =  filename.split('.')[0]
 	gb_file_test = os.path.join('/home/vdp5/data/gene_finder/SAMEA2376790_genbank/SAMEA2376790_gb', filename)
-	fullpathnew = os.mkdir.join('/home/vdp5/data/gene_finder/vir_bychrom', chromname)
-	output_fle = open(fullpathnew, 'w')
+	fullpathnew = os.mkdir(os.path.join('/home/vdp5/data/gene_finder/vir_bychrom', chromname))
+	os.chdir(os.path.join('/home/vdp5/data/gene_finder/vir_bychrom', chromname))
 	data = SeqIO.read(open(gb_file_test,"r"), "genbank")
 	for alpha in data.features:
 		tmp =  alpha.qualifiers
 		if 'product' in tmp:
 			if 'VIR' in tmp['product'][0]:
 				sequence =  alpha.extract(data.seq)
-				output_fle.write('> %s | %s\n' %(tmp['locus_tag'][0], chromname))
+				output_fle = open('%s_%s.fasta' %(tmp['locus_tag'][0], chromname), 'w')
+				output_fle.write('>%s | %s\n' %(tmp['locus_tag'][0], chromname))
 				output_fle.write('%s\n' %(sequence))
+				output_fle.close()
 
 
 
