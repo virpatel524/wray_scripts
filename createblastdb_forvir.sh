4fde9894e78533cbeee8c1d3e775cea99695df4a
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-26 11:06:04 -0400

    routine upload

--- createblastdb_forvir.sh
+++ createblastdb_forvir.sh
@@ -1,2 +1,4 @@
 cd /home/vdp5/data/gene_finder/vir_bychrom/allvir_blastdb
 makeblastdb -in allgenes.fasta -parse_seqids -dbtype nucl
+cd /home/vdp5/data/gene_finder/vir_bychrom/allvir_protblastdb
+makeblastdb -in allprot.fasta -parse_seqids -dbtype prot 
