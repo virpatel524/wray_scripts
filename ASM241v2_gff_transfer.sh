d0ac063ac0b2c2ff9a160295347dfc8dd916a33e
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-27 00:20:11 -0400

    routine upload

--- ASM241v2_gff_transfer.sh
+++ ASM241v2_gff_transfer.sh
@@ -1,3 +1,3 @@
 cd /home/vdp5/tmp
 
-gffread -O -w exons.fasta -g /home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic.fasta /home/vdp5/data/gff_data/GCF_000002415.2_ASM241v2_genomic.gff
+gffread -G -w exons.fasta -g /home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic.fasta /home/vdp5/data/gff_data/GCF_000002415.2_ASM241v2_genomic.gff
