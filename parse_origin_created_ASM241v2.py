5c889265dda3e3d03be41e61600b95da4dcce34f
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-26 23:44:34 -0400

    routine upload

--- parse_origin_created_ASM241v2.py
+++ parse_origin_created_ASM241v2.py
@@ -25,7 +25,7 @@ for alpha in data:
 		continue
 
 
-base = '/home/vdp5/data/gene_finder/origin_sequences/splitup/ASM241v2'
+base = '/home/vdp5/data/gene_finder/origin_sequences/splitup/ASM241v2/'
 
 for alpha in chrom2seq:
 	output = base + alpha + '.txt'
