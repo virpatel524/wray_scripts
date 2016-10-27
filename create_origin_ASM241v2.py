a919eebfb11bc9aa323cbb208084faf0f1641b19
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-26 23:13:54 -0400

    routine upload

---
+++ create_origin_ASM241v2.py
@@ -0,0 +1,20 @@
+import Bio
+from Bio import SeqIO
+from Bio.Alphabet import generic_dna
+
+input_handle = open("/home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_shortid.fasta", "r")
+output_handle = open("/home/vdp5/data/gene_finder/origin_sequences/all_origin.gb", "w")
+
+sequences = list(SeqIO.parse(input_handle, "fasta"))
+
+#asign generic_dna or generic_protein
+for seq in sequences:
+  seq.seq.alphabet = generic_dna
+
+count = SeqIO.write(sequences, output_handle, "genbank")
+
+output_handle.close()
+input_handle.close()
+print "Coverted %i records" % count
+
+
