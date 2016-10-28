bc73d47b775ca3db09a5f9ec7f1c51df446cc5fb
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-28 13:03:38 -0400

    routine upload

--- edit_chrom_ASM241v2fasta.py
+++ edit_chrom_ASM241v2fasta.py
@@ -1,3 +1,12 @@
 import csv, os
 
 input_data = list(csv.reader(open('/home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic_originalchromid.fasta','r'),delimiter='\t'))
+output_data = open('/home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic_edited_originalchromid.fasta','w')
+
+
+for alpha in input_data:
+	if alpha[0][0] == '>':
+		print alpha[0].split(' ')[0]
+
+
+output_data.close()
