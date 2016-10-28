ef3fd36ec17c1563ce934ccb6102d657337e29e1
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-28 13:44:30 -0400

    routine upload

--- characterize_portillioG.py
+++ characterize_portillioG.py
@@ -2,12 +2,16 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 
-import csv, os, sys
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import csv, os, sys
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 input_data = list(csv.reader(open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/ASM214v2_genesexons.fasta'),delimiter='\t'))
 portillo_g = zip(*list(csv.reader(open('/home/vdp5/data/other_data/portillo_Gfamily.txt'),delimiter='\t')))[0]
+
+portillodata = {}
+
+
 
 gene2numexon = {}
 
@@ -17,10 +21,13 @@ for index, data in enumerate(input_data):
 	for gene in portillo_g:
 		if gene in data[0]:
 			gene2numexon.setdefault(gene, []).append(1)
+			portillodata[data[0]] = input_data[index + 1][0]
 
 for gene in gene2numexon:
 	gene2numexon[gene] = sum(gene2numexon[gene])
 
-sns.distplot(gene2numexon.values(), kde=False, color='b')
-plt.savefig('/home/vdp5/figures/portilloG_numberexons.pdf',bbox_inches='tight')
-plt.close()
+# sns.countplot(gene2numexon.values(), color='b')
+# plt.savefig('/home/vdp5/figures/portilloG_numberexons.pdf',bbox_inches='tight')
+# plt.close()
+
+print portillodata
