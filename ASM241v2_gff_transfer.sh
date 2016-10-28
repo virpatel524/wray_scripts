fd5685c35cc4430359d45e2e0761195a3788517a
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-27 23:20:44 -0400

    routine upload

--- ASM241v2_gff_transfer.sh
+++ ASM241v2_gff_transfer.sh
@@ -3,4 +3,4 @@ python /home/vdp5/scripts/gff3_to_bed_converter.py -q /home/vdp5/data/gff_data/G
 
 
 
-bedtools getfasta -fi /home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic.fasta -fo /home/vdp5/data/gene_finder/ASM241v2_exongrab/ASM241v2_exons.fasta -bed /home/vdp5/data/gff_data/GCF_000002415.2_ASM241v2_genomic_bedformat.bed -split -name 
+bedtools getfasta -fi /home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic.fasta -fo /home/vdp5/data/gene_finder/ASM241v2_exongrab/ASM241v2_exons.fasta -bed /home/vdp5/data/gff_data/GCF_000002415.2_ASM241v2_genomic_bedformat.bed -split  
