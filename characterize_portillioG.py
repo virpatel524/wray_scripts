35e9e9ee33a8e8e57ac30dd69e5f35357a701e16
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-28 13:34:15 -0400

    routine upload

--- characterize_portillioG.py
+++ characterize_portillioG.py
@@ -1,6 +1,19 @@
 import csv, os, sys
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 input_data = list(csv.reader(open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/ASM214v2_genesexons.fasta'),delimiter='\t'))
 portillo_g = zip(*list(csv.reader(open('/home/vdp5/data/other_data/portillo_Gfamily.txt'),delimiter='\t')))[0]
 
-print portillo_g
+gene2numexon = {}
+
+for index, data in enumerate(input_data):
+	if data[0][0] != '>':
+		continue
+	for gene in portillo_g:
+		if gene in data[0]:
+			gene2numexon.setdefault(gene, []).append(1)
+
+for gene in gene2numexon:
+	gene[gene2numexon] = sum(gene2numexon[gene])
+
