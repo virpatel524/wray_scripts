b4963c30c3fa3f13a0b1cb9e5c64eb82c6256f56
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-28 11:52:08 -0400

    routine upload

--- GCF_gff2bed.py
+++ GCF_gff2bed.py
@@ -1,5 +1,6 @@
 import csv, os
 
 input_data = list(csv.reader(open('/home/vdp5/data/gff_data/GCF_000002415.2_ASM241v2_genomic.gff'),delimiter='\t'))
-
-print input_data
+print len(input_data)
+input_data = [a for a in input_data if a[0][0] != '#']
+print len(input_data)
