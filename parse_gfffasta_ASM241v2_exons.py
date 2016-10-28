3762531bff69dbca7be5dc233c44447ba678a227
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-28 12:27:57 -0400

    routine upload

--- parse_gfffasta_ASM241v2_exons.py
+++ parse_gfffasta_ASM241v2_exons.py
@@ -3,7 +3,7 @@ csv.field_size_limit(sys.maxsize)
 
 
 input_data = list(csv.reader(open('/home/vdp5/data/gene_finder/ASM241v2_exongrab/ASM241v2_exons.fasta'),delimiter='\t'))
-
+output_data = open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/' + 'ASM214v2_genesexons.fasta', 'w')
 gene2exon = {}
 
 for index, data in enumerate(input_data):
@@ -12,5 +12,9 @@ for index, data in enumerate(input_data):
 	tmp = data[0].split(';')
 	tmp[0] = tmp[0][1:]
 	if tmp[0] == 'exon':
+		ident = tmp[1][5:]
 		gene = tmp[4].split(' ')[-1][:-1]
-		gene2exon.setdefault(gene, []).append(input_data[index + 1])
+		gene2exon.setdefault(gene, []).append([ident, input_data[index + 1]])
+
+
+
