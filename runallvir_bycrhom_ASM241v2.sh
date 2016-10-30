a60111ea2267dd97ac0e332b75e8b4a27c8c131d
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-30 11:18:48 -0400

    routine upload

--- runallvir_bycrhom_ASM241v2.sh
+++ runallvir_bycrhom_ASM241v2.sh
@@ -4,10 +4,9 @@ for alpha in ${iterarray[@]}; do
 		for delta in $beta*; do
 			cd /home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes
 			IFS=/ read -ra newarray <<< $delta
-			IFS=. read -ra newerarray <<< ${newarray[-1]}
+			IFS=fasta read -ra newerarray <<< ${newarray[-1]}
 			IFS=_ read -ra chromarray <<< ${newerarray[0]}
 			chrom=${chromarray[-1]}
-			echo $chrom
 			# mkdir ${newerarray[0]}
 			# cd ${newerarray[0]}
 			# splign -query $delta -subj /home/vdp5/data/cdna_analysis_SAMEA2376790/fasta_PV01/$chrom.fasta -aln ${newerarray[0]}_splign
