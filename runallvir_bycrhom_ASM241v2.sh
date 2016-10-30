1ded351cf455c6890ba351784defd89c84f0d7cb
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-30 11:33:48 -0400

    routine upload

--- runallvir_bycrhom_ASM241v2.sh
+++ runallvir_bycrhom_ASM241v2.sh
@@ -2,7 +2,7 @@ iterarray=(  01 02 03 04 05 06 07 08 09 10 11 12 13 14  )
 for alpha in ${iterarray[@]}; do
 	for beta in /home/vdp5/data/gene_finder/vir_bychrom_ASM241v2/*/; do
 		for delta in $beta*; do
-			cd /home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes
+			cd /home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes
 			IFS=/ read -ra newarray <<< $delta
 			IFS=fasta read -ra newerarray <<< ${newarray[-1]}
 			fullname=${newerarray[0]}
