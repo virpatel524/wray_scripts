899533f84e39f76cdead70057d3be5716086072b
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-30 11:07:38 -0400

    routine upload

--- runallvir_bycrhom_ASM241v2.sh
+++ runallvir_bycrhom_ASM241v2.sh
@@ -1,13 +1,14 @@
 iterarray=(  01 02 03 04 05 06 07 08 09 10 11 12 13 14  )
 for alpha in ${iterarray[@]}; do
-	for beta in /home/vdp5/data/gene_finder/vir_bychrom_SAMEA2376790/chrom$alpha/*; do
-		cd /home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes
-		IFS=/ read -ra newarray <<< $beta
-		IFS=. read -ra newerarray <<< ${newarray[-1]}
-		IFS=_ read -ra chromarray <<< ${newerarray[0]}
-		chrom=${chromarray[-1]}
-		mkdir ${newerarray[0]}
-		cd ${newerarray[0]}
-		splign -query $beta -subj /home/vdp5/data/cdna_analysis_SAMEA2376790/fasta_PV01/$chrom.fasta -aln ${newerarray[0]}_splign
+	for beta in /home/vdp5/data/gene_finder/vir_bychrom_ASM241_v2/*; do
+		echo $beta
+		# cd /home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes
+		# IFS=/ read -ra newarray <<< $beta
+		# IFS=. read -ra newerarray <<< ${newarray[-1]}
+		# IFS=_ read -ra chromarray <<< ${newerarray[0]}
+		# chrom=${chromarray[-1]}
+		# mkdir ${newerarray[0]}
+		# cd ${newerarray[0]}
+		# splign -query $beta -subj /home/vdp5/data/cdna_analysis_SAMEA2376790/fasta_PV01/$chrom.fasta -aln ${newerarray[0]}_splign
 	done
 done
