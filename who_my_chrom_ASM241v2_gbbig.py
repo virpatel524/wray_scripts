5b8a0150006bf28dd0c7f41e86c4bd7679953af7
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 11:14:18 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.py
+++ who_my_chrom_ASM241v2_gbbig.py
@@ -24,14 +24,20 @@ gene2chromfle = open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/gene2chro
 newlst = []
 
 os.chdir('/home/vdp5/data/cdna_analysis_ASM241_v2/fasta_ASM241_v2')
+
+
+
+
 for gene in gene2seq:
 	seqinq = str(gene2seq[gene])
 	seqinqr = str(gene2seq[gene].reverse_complement())
 	bash_process_forward = 'grep -i -r -l "{}" *'.format(seqinq)
 	bash_process_reverse = 'grep -i -r -l "{}" *'.format(seqinqr)
 
-	output = subprocess.check_output("grep 'sin' *", shell=True)
-	newlst.append(output)
+	output = subprocess.check_output(bash_process_forward, shell=True)
+	output = subprocess.check_output(bash_process_reverse, shell=True)
 
-print newlst
+
+	break
+
 
