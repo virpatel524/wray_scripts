53795ed9f90162387ab24d4b3f59103c961f26d9
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 11:06:21 -0400

    routine upload

--- runallvir_all_ASM241v2.sh
+++ runallvir_all_ASM241v2.sh
@@ -4,11 +4,6 @@ for delta in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
 	IFS=fasta read -ra newerarray <<< ${newarray[-1]}
 	fullname=${newerarray[0]}
 	fullname=${fullname::-1}
-	IFS=_ read -ra chromarray <<< ${newerarray[0]}
-	chrom=${chromarray[-2]}_${chromarray[-1]}
-	chrom=${chrom::-1}
-	mkdir ${fullname}
-	cd ${fullname}
 	splign -query $delta -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_ASM241_v2/$chrom.fasta -aln ${fullname}_splign
 done
 
