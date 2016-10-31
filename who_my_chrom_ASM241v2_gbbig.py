556b5fb1764a5cbd63f3e4c3a43b6eb185cb56ee
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 10:47:33 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.py
+++ who_my_chrom_ASM241v2_gbbig.py
@@ -2,13 +2,12 @@ import os, csv, sys
 
 data = list(csv.reader(open('/home/vdp5/data/gene_finder/vir_all_ASM241v2/allgenes.txt'),delimiter='\t'))
 
-
+data = [a[0] for a in data]
 chrom2seq = {}
-
-for index, alpah in data:
+for index, alpah in enumerate(data):
 	if index % 2 != 0:
 		continue
 	else:
-		chrom2seq[alpah[0]] = data[index][0]
+		chrom2seq[alpah[1:]] = data[index + 1]
 
-print chrom2seq
+print chrom2seq['PVX_008085_nochromid']
