241a6680e221a9b1c1992c3ef02eab1ab02f4f57
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 10:46:25 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.py
+++ who_my_chrom_ASM241v2_gbbig.py
@@ -1,4 +1,14 @@
 import os, csv, sys
 
 data = list(csv.reader(open('/home/vdp5/data/gene_finder/vir_all_ASM241v2/allgenes.txt'),delimiter='\t'))
-print data
+
+
+chrom2seq = {}
+
+for index, alpah in data:
+	if index % 2 != 0:
+		continue
+	else:
+		chrom2seq[alpah[0]] = data[index][0]
+
+print chrom2seq
