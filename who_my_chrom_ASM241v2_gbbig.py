b115c2559019bd4daebcae524e51ef7cbbee53fc
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 10:54:11 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.py
+++ who_my_chrom_ASM241v2_gbbig.py
@@ -2,6 +2,7 @@ import os, csv, sys
 
 import Bio
 from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
 
 
 data = list(csv.reader(open('/home/vdp5/data/gene_finder/vir_all_ASM241v2/allgenes.txt'),delimiter='\t'))
@@ -12,11 +13,13 @@ for index, alpah in enumerate(data):
 	if index % 2 != 0:
 		continue
 	else:
-		gene2seq[alpah[1:]] = data[index + 1]
+		gene2seq[alpah[1:]] = seq(absdata[index + 1], generic_dna)
 
 
 gene2chromfle = open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/gene2chrom.txt','w')
 
 os.chdir('/home/vdp5/data/cdna_analysis_ASM241_v2/fasta_ASM241_v2')
 for gene in gene2seq:
-	bash_process = 'grep -i -r -l '
+	seqinq = gene2seq[gene]
+	seqinqr = 
+	bash_process = 'grep -i -r -l "{}" *'.format()
