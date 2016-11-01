eaf681f6d8f547d3232c82dbbaa654270e022285
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 21:32:35 -0400

    routine upload

--- get_complement.py
+++ get_complement.py
@@ -1,6 +1,10 @@
 import os, sys, csv, argparse
 
+import Bio
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+
 parser = argparse.ArgumentParser();
 parser.add_argument('-seq')
 args = parser.parse_args()
-print vars(args)
+
