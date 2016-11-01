f21f9f9085726f436bef17c311fa7fdb248d925c
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 21:33:42 -0400

    routine upload

--- get_complement.py
+++ get_complement.py
@@ -7,4 +7,6 @@ from Bio.Alphabet import generic_dna
 parser = argparse.ArgumentParser();
 parser.add_argument('-seq')
 args = parser.parse_args()
-
+args = vars(args)
+dna = Seq(args['seq'])
+print dna.reverse_complement()
