7a818a3c0cc1226e22baa261b4df0456ee420d4a
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 21:40:15 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.sh
+++ who_my_chrom_ASM241v2_gbbig.sh
@@ -4,5 +4,6 @@ for alpha in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
 	reverse=$(python /home/vdp5/scripts/get_complement.py -seq $forward)
 	cd /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_ASM241_v2
 	forwardgrep=$(grep -i -r -l $forward *)
-	
+	reversegrep=$(grep -i -r -l $reverse *)
+	printf "%s\t%s\t%s\n"
 done
