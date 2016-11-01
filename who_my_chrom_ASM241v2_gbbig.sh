2e3db4b6a06bdabb47342e2b9073cd127e932d0e
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-10-31 21:34:50 -0400

    routine upload

--- who_my_chrom_ASM241v2_gbbig.sh
+++ who_my_chrom_ASM241v2_gbbig.sh
@@ -1,4 +1,6 @@
 for alpha in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
 	genemame=$(basename -s .fasta $alpha)
-	tail -n+2 $alpha | head -n1
+	forward=$(tail -n+2 $alpha | head -n1)
+	reverse=$(python /home/vdp5/scripts/get_complement.py -seq $forward)
+	echo $reverse
 done
