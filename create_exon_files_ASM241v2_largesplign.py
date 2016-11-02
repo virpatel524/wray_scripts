3953f93d42fadb620c84ea0b52c4fab5cfffbdf9
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-02 00:02:57 -0400

    routine upload

--- create_exon_files_ASM241v2_largesplign.py
+++ create_exon_files_ASM241v2_largesplign.py
@@ -3,4 +3,9 @@ import os
 import sys
 
 
-imput_dir = '/home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes'
+input_dir = '/home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes'
+outputfle = open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/ASM214v2_genesexons.fasta', 'w')
+
+
+for subdir, dirs, files in os.walk(input_dir):
+	print files
