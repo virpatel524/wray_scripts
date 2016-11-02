f52bd680d86c6236d7608aee1f9e712abf263b4c
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-01 23:28:36 -0400

    routine upload

--- runallvir_bychrom.sh
+++ runallvir_bychrom.sh
@@ -1,4 +1,4 @@
 for vir in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
 	genename=$(basename --suffix=.fasta $vir)
-	splign -query $vir -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_full_exon_determine.fasta -aln 
+	splign -query $vir -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_full_exon_determine.fasta -aln /home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes/$genename.splign
 done
