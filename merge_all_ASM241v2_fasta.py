2999fd8cb5150ad05a9371553b0c081bf9c43778
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-01 23:03:48 -0400

    routine upload

--- merge_all_ASM241v2_fasta.py
+++ merge_all_ASM241v2_fasta.py
@@ -3,4 +3,10 @@ import os, sys, csv
 
 data = list(csv.reader(open('/home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2_genomic_edited_originalchromid.fasta'),delimiter='\t')) 
 
-print len(data)
+dnabank = ''
+
+for index, alpha in enumerate(data):
+	if index % 2 == 0:
+		continue
+	else:
+		print alpha
