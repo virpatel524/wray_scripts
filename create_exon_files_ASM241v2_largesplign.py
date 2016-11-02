a501b19a36e2f8f9b36afcfaef17726547b77315
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-02 00:18:07 -0400

    routine upload

--- create_exon_files_ASM241v2_largesplign.py
+++ create_exon_files_ASM241v2_largesplign.py
@@ -6,23 +6,33 @@ flag_file = open('/home/vdp5/data/cdna_analysis_SAMEA2376790/flagged_genes.txt',
 input_dir = '/home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes'
 exitfle = open('/home/vdp5/data/gene_finder/ASM241v2_genes_exons/ASM214v2_genesexons.fasta', 'w')
 
+
+
+def process_data(alpha_data):
+	exonnums = []
+	tmplst = []
+	for alpha in tmp:
+		if len(alpha) == 0: continue
+		if 'Exon' in alpha[0]:
+			data = alpha[0].split(' ')
+			if float(data[-1]) < 0.95:
+				return 'nope', 'nope'
+			tmplst.append('exon{}:{}'.format(data[2], data[3].split('(')[-1].split(',')[0]))
+			exonnums.append(int(data[2]))
+	return tmplst, exonnums
+
+
+
+
 counter = 0
 for subdir, dirs, files in os.walk(input_dir):
 	outerloop: 
 	for splignout in files:
 		tmp = list(csv.reader(open(os.path.join(input_dir, splignout)),delimiter='\t'))
 		gene='_'.join(splignout.split('_')[:2])
-		exonnums = []
-		tmplst = []
-		for alpha in tmp:
-			if len(alpha) == 0: continue
-			if 'Exon' in alpha[0]:
-				data = alpha[0].split(' ')
-				if float(data[-1]) < 0.95:
-					break outerloop;
-				tmplst.append('exon{}:{}'.format(data[2], data[3].split('(')[-1].split(',')[0]))
+		tmplst, exonnums = process_data(alpha)
 
-				exonnums.append(int(data[2]))
+
 		tmplst = sorted(tmplst)
 		if len(tmplst) != len(list(set(exonnums))):
 			flag_file.write('{}\n'.format(gene))
