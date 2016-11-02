d683cf4e099fb7cee6d02b19034d5cfc8853cf7d
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-01 23:04:55 -0400

    routine upload

--- merge_all_ASM241v2_fasta.py
+++ merge_all_ASM241v2_fasta.py
@@ -6,7 +6,7 @@ data = list(csv.reader(open('/home/vdp5/data/vivax_2009/GCA_000002415.2_ASM241v2
 dnabank = ''
 
 for index, alpha in enumerate(data):
-	if index % 2 == 0:
+	if alpha[0][0] == '>':
 		continue
 	else:
 		print alpha
