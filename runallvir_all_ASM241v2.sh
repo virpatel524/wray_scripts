8d15a792f7368b8b66c4ecfc9a85605f76fc5edd
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-01 23:29:43 -0400

    routine upload

--- runallvir_all_ASM241v2.sh
+++ runallvir_all_ASM241v2.sh
@@ -1,9 +1,4 @@
-for delta in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
-	cd /home/vdp5/data/cdna_analysis_ASM241_v2/prechromidsplign/spliceoutput
-	IFS=/ read -ra newarray <<< $delta
-	IFS=fasta read -ra newerarray <<< ${newarray[-1]}
-	fullname=${newerarray[0]}
-	fullname=${fullname::-1}
-	splign -query $delta -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_ASM241_v2/$chrom.fasta -aln ${fullname}_splign
+for vir in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
+	genename=$(basename --suffix=.fasta $vir)
+	splign -query $vir -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_full_exon_determine.fasta -aln /home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes/$genename.splign
 done
-
--- runallvir_bychrom.sh
+++ runallvir_bychrom.sh
@@ -1,4 +1,13 @@
-for vir in /home/vdp5/data/gene_finder/vir_all_ASM241v2/all_split/*; do
-	genename=$(basename --suffix=.fasta $vir)
-	splign -query $vir -subj /home/vdp5/data/cdna_analysis_ASM241_v2/fasta_full_exon_determine.fasta -aln /home/vdp5/data/cdna_analysis_ASM241_v2/vir_genes/$genename.splign
+iterarray=(  01 02 03 04 05 06 07 08 09 10 11 12 13 14  )
+for alpha in ${iterarray[@]}; do
+	for beta in /home/vdp5/data/gene_finder/vir_bychrom_SAMEA2376790/chrom$alpha/*; do
+		cd /home/vdp5/data/cdna_analysis_SAMEA2376790/vir_genes
+		IFS=/ read -ra newarray <<< $beta
+		IFS=. read -ra newerarray <<< ${newarray[-1]}
+		IFS=_ read -ra chromarray <<< ${newerarray[0]}
+		chrom=${chromarray[-1]}
+		mkdir ${newerarray[0]}
+		cd ${newerarray[0]}
+		splign -query $beta -subj /home/vdp5/data/cdna_analysis_SAMEA2376790/fasta_PV01/$chrom.fasta -aln ${newerarray[0]}_splign
+	done
 done
