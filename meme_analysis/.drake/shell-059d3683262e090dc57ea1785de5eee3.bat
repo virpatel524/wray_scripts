4082f52022ddb946c4864c17dd11a0087becf89d
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 12:41:15 -0400

    routine upload

---
+++ meme_analysis/.drake/shell-059d3683262e090dc57ea1785de5eee3.bat
@@ -0,0 +1,2 @@
+mast -oc /home/vdp5/data/hhmm_data/mast_analysis4_newplasmodb13_exon2_dreme_corradjust $INPUT0 $INPUT1
+echo "complete" > stampcomplete/mast_analysis4_newplasmodb13_exon2_dreme_corradjust.log
