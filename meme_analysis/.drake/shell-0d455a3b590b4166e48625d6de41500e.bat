8541b4c27d93bea418f05098775448f96bdfcf4a
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 12:21:09 -0400

    routine upload

---
+++ meme_analysis/.drake/shell-0d455a3b590b4166e48625d6de41500e.bat
@@ -0,0 +1,2 @@
+python /home/vdp5/scripts/general_purpose/split_exon_fasta.py -fasta /nfs/wraycompute/diana/var/var_falciparum_cds.fa -outdir /home/vdp5/data/falc_analysis_pacbio/diana_falc_var_exonsplit
+echo "complete" > stampcomplete/diana_var_exonsplit.log
