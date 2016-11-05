4252dd5b6ebea0b4442e946f9f091a99a5c59e1f
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 12:08:39 -0400

    routine upload

--- general_purpose/split_exon_fasta.py
+++ general_purpose/split_exon_fasta.py
@@ -1,2 +1,8 @@
 import csv, os, args, sys
 
+parser = argparse.ArgumentParser();
+parser.add_argument('-fasta')
+parser.add_argument('-outdir')
+args = parser.parse_args()
+args = vars(args)
+fastadata = list(csv.reader(open(['fasta']),delimiter='\t'))
