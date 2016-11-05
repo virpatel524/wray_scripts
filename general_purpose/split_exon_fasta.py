ddc23370413043c81a626eafa083c906e2c4a782
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 12:12:02 -0400

    routine upload

--- general_purpose/split_exon_fasta.py
+++ general_purpose/split_exon_fasta.py
@@ -14,11 +14,12 @@ curgene = 'fadsjfkdsa'
 
 for alpha in fastadata:
 	if alpha[0][0] == '>':
-		curgene = alpha[0]
+		curgene = alpha[0] + '\n'
 	else:
 		gene2seq.setdefault(curgene, []).append(alpha[0])
 
 
 for gene in gene2seq:
-	
+	gene2seq[gene] = ''.join(gene2seq[gene]) + '\n'
+
 
