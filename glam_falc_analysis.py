dbca03274dd2c7afb34ca3a52b9b041dd801cad1
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 17:43:01 -0400

    routine upload

--- glam_falc_analysis.py
+++ glam_falc_analysis.py
@@ -1,15 +1,28 @@
 import sys, os, argparse, csv
 
+
+
+def make_bash_file
+
+
+
+
+
+
+
+
+
 parser = argparse.ArgumentParser();
 parser.add_argument('-exons', nargs='+')
 parser.add_argument('-targetproteins')
+parser.add_argument('-outputdir')
 args = parser.parse_args()  
 args = vars(args)
 
 
 exonsraw = args['exons']
-print exonsraw
 targetprotrwaw = args['targetproteins']
+outputdir = args['outputdir']
+
 
 temperatures = [0.11, 0.2, 0.4, 0.7, 1, 1.2, 1.4, 1.7, 2, 3, 5]
-
