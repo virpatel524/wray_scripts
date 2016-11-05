ff35ed3138fd5c01b9b4b84aef82fc69fdda7f30
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 17:04:40 -0400

    routine upload

--- glam_falc_analysis.py
+++ glam_falc_analysis.py
@@ -1,2 +1,15 @@
 import sys, os, argparse, csv
 
+parser = argparse.ArgumentParser();
+parser.add_argument('-exon1')
+parser.add_argument('-exon2')
+parser.add_argument('-targetproteins')
+args = parser.parse_args()  
+args = vars(args)
+
+exon1fle = list(csv.reader(open(args['exon1']),delimiter='\t'))
+exon2fle = list(csv.reader(open(args['exon2']),delimiter='\t'))
+targetprotfle = list(csv.reader(open(args['targetprotiens']),delimiter='\t'))
+
+
+
