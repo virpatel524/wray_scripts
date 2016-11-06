479002bc914b13d7611abb89132a350786c6ef76
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-06 00:57:13 -0400

    routine upload

--- glam_falc_analysis.py
+++ glam_falc_analysis.py
@@ -132,6 +132,7 @@ outputdir = args['outputdir']
 temperatures = [0.11, 0.2, 0.4, 0.7, 1, 1.2, 1.4, 1.7, 2, 3, 5]
 
 relevantpaths = make_bash_file(temperatures, exonsraw, targetprotrwaw, outputdir)
+print 'bash {}'.format(os.path.join(outputdir, 'runscript.sh'))
 processrun = subprocess.Popen(['bash {}'.format(os.path.join(outputdir, 'runscript.sh'))])
 processrun.wait() 
 
