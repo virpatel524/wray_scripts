d5d4dfc883f3b9d2b0a78671c3bb11a4d0a9f87c
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-05 21:59:06 -0400

    routine upload

--- glam_falc_analysis.py
+++ glam_falc_analysis.py
@@ -1,9 +1,12 @@
 import sys, os, argparse, csv, shutil
 
 
+
 def parse_motif_file(dirpath):
+	curscore = 'f'
 	motif_file = list(csv.reader(open(os.path.join(dirpath, 'glam2.txt')),delimiter='\t'))
 	
+
 
 
 
