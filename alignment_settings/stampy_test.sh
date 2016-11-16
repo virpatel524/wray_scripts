3e528fea1260f9cac31f14055c89034615eaea01
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2016-11-15 21:03:21 -0500

    routine upload

--- alignment_settings/stampy_test.sh
+++ alignment_settings/stampy_test.sh
@@ -1,1 +1,1 @@
-stampy.py 
+stampy.py -o /home/vdp5/data/optimize_alignment_tests/ -f sam -T -g /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta -h /home/vdp5/data/salvador_vivax_asia_2016/first-SAMEA2376790/pvivax_sal1_SAMEA2376790.fasta
